b0e53cea8a5e755bcead6f4fff1a2f89f508a435
Hugo Müller-Downing <hugom@squareup.com>
2024-10-25 15:49:11 +1100

    Exposes state on InvalidStateTransition
    Allows consumers to access the state for which the value could not be
    transitioned.
    
    Useful for partitioning transition failures into categories, e.g.
    known failures, and unknown or unexpected failures.

--- lib/src/test/kotlin/app/cash/kfsm/InvalidStateTransitionTest.kt
+++ lib/src/test/kotlin/app/cash/kfsm/InvalidStateTransitionTest.kt
@@ -1,6 +1,7 @@
 package app.cash.kfsm
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 
 class InvalidStateTransitionTest : StringSpec({
@@ -12,5 +13,11 @@ class InvalidStateTransitionTest : StringSpec({
   "with many from-states has correct message" {
     InvalidStateTransition(LetterTransition(States(C, B), D), Letter(E)).message shouldBe
       "Value cannot transition {B, C} to D, because it is currently E. [value=Letter(state=E)]"
+  }
+
+  "exposes transition and state" {
+    val error = InvalidStateTransition(LetterTransition(A, B), Letter(E))
+
+    error.getState<Char>() shouldBe E
   }
 })
