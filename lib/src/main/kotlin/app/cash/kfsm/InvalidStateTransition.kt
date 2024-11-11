82923b713d089eacb06ef2f039bd726410e5781f
Hugo Müller-Downing <hugom@squareup.com>
2024-11-12 10:51:05 +1100

    Makes transition private in InvalidStateTransition

--- lib/src/main/kotlin/app/cash/kfsm/InvalidStateTransition.kt
+++ lib/src/main/kotlin/app/cash/kfsm/InvalidStateTransition.kt
@@ -1,6 +1,6 @@
 package app.cash.kfsm
 
-data class InvalidStateTransition(val transition: Transition<*, *>, val value: Value<*, *>) : Exception(
+data class InvalidStateTransition(private val transition: Transition<*, *>, val value: Value<*, *>) : Exception(
   "Value cannot transition ${
     transition.from.set.toList().sortedBy { it.toString() }.joinToString(", ", prefix = "{", postfix = "}")
   } to ${transition.to}, because it is currently ${value.state}. [value=$value]"
