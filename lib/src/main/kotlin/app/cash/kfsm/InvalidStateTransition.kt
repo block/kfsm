3dd340f96bf89f5d9770f53390eec2d5a5ae0e71
Hugo <github@hu.md>
2024-11-12 10:57:01 +1100

    Exposes transition and value on InvalidStateTransition (#55)
    * Exposes state on InvalidStateTransition
    
    Allows consumers to access the state for which the value could not be
    transitioned.
    
    Useful for partitioning transition failures into categories, e.g.
    known failures, and unknown or unexpected failures.
    
    * Makes transition private in InvalidStateTransition

--- lib/src/main/kotlin/app/cash/kfsm/InvalidStateTransition.kt
+++ lib/src/main/kotlin/app/cash/kfsm/InvalidStateTransition.kt
@@ -1,7 +1,13 @@
 package app.cash.kfsm
 
-class InvalidStateTransition(transition: Transition<*, *>, value: Value<*, *>) : Exception(
+data class InvalidStateTransition(private val transition: Transition<*, *>, val value: Value<*, *>) : Exception(
   "Value cannot transition ${
     transition.from.set.toList().sortedBy { it.toString() }.joinToString(", ", prefix = "{", postfix = "}")
   } to ${transition.to}, because it is currently ${value.state}. [value=$value]"
-)
+) {
+  /**
+   * Get the state of the underlying value.
+   * This reflects the state that could not be transitioned successfully.
+   */
+  inline fun <reified S: State<S>> getState(): S? = value.state as? S
+}
