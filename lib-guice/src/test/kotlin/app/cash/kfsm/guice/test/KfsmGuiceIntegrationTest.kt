c7ad46539210acebbe6cfa7168764c6ae9d78b85
Jeremy Mawson <jmawson@block.xyz>
2025-04-23 09:48:15 +1000

    Added README

--- lib-guice/src/test/kotlin/app/cash/kfsm/guice/test/KfsmGuiceIntegrationTest.kt
+++ lib-guice/src/test/kotlin/app/cash/kfsm/guice/test/KfsmGuiceIntegrationTest.kt
@@ -4,41 +4,46 @@ import app.cash.kfsm.guice.StateMachine
 import com.google.inject.Guice
 import com.google.inject.Key
 import com.google.inject.TypeLiteral
-import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 
-class KfsmGuiceIntegrationTest {
-    private val injector = Guice.createInjector(TestModule())
-    private val stateMachine = injector.getInstance(
+class KfsmGuiceIntegrationTest : StringSpec({
+    val injector = Guice.createInjector(TestModule())
+    val stateMachine = injector.getInstance(
         Key.get(object : TypeLiteral<StateMachine<TestValue, TestState>>() {})
     )
+    val startValue = TestValue(TestState.START)
 
-    @Test
-    fun `test state machine integration`() {
-        // Start with a value in START state
-        val startValue = TestValue(TestState.START)
-        
-        // Get available transitions
-        val availableTransitions = stateMachine.getAvailableTransitions(startValue.state)
-        assertEquals(1, availableTransitions.size)
-        assertTrue(availableTransitions.any { it is StartToMiddle })
-        
-        // Execute the transition
-        val middleValue = stateMachine.execute(startValue, stateMachine.getTransition<StartToMiddle>()).getOrThrow()
-        assertEquals(TestState.MIDDLE, middleValue.state)
-        
-        // Get next available transitions
-        val nextTransitions = stateMachine.getAvailableTransitions(middleValue.state)
-        assertEquals(1, nextTransitions.size)
-        assertTrue(nextTransitions.any { it is MiddleToEnd })
-        
-        // Execute final transition
-        val endValue = stateMachine.execute(middleValue, stateMachine.getTransition<MiddleToEnd>()).getOrThrow()
-        assertEquals(TestState.END, endValue.state)
-        
-        // Verify no more transitions available
-        val finalTransitions = stateMachine.getAvailableTransitions(endValue.state)
-        assertTrue(finalTransitions.isEmpty())
+    "START state should have exactly one available transition of type StartToMiddle" {
+        val transitions = stateMachine.getAvailableTransitions(startValue.state)
+        transitions shouldHaveSize 1
+        transitions.first().shouldBeInstanceOf<StartToMiddle>()
     }
-} 
+
+    "executing StartToMiddle transition should result in MIDDLE state" {
+        val middleValue = stateMachine.execute(startValue, stateMachine.getTransition<StartToMiddle>()).getOrThrow()
+        middleValue.state shouldBe TestState.MIDDLE
+    }
+
+    "MIDDLE state should have exactly one available transition of type MiddleToEnd" {
+        val middleValue = stateMachine.execute(startValue, stateMachine.getTransition<StartToMiddle>()).getOrThrow()
+        val transitions = stateMachine.getAvailableTransitions(middleValue.state)
+        transitions shouldHaveSize 1
+        transitions.first().shouldBeInstanceOf<MiddleToEnd>()
+    }
+
+    "executing MiddleToEnd transition should result in END state" {
+        val middleValue = stateMachine.execute(startValue, stateMachine.getTransition<StartToMiddle>()).getOrThrow()
+        val endValue = stateMachine.execute(middleValue, stateMachine.getTransition<MiddleToEnd>()).getOrThrow()
+        endValue.state shouldBe TestState.END
+    }
+
+    "END state should have no available transitions" {
+        val middleValue = stateMachine.execute(startValue, stateMachine.getTransition<StartToMiddle>()).getOrThrow()
+        val endValue = stateMachine.execute(middleValue, stateMachine.getTransition<MiddleToEnd>()).getOrThrow()
+        val transitions = stateMachine.getAvailableTransitions(endValue.state)
+        transitions shouldHaveSize 0
+    }
+}) 
