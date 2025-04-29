f0af648f4cd492167ce142e3d7add60791474f8c
Jeremy Mawson <jmawson@block.xyz>
2025-04-29 11:33:21 +1000

    Reflections lib should be part of API for guice

--- lib-guice/build.gradle.kts
+++ lib-guice/build.gradle.kts
@@ -8,10 +8,10 @@ dependencies {
     api(project(":lib"))
     
     // Guice for dependency injection
-    api("com.google.inject:guice:7.0.0")
+    api(rootProject.libs.guice)
     
     // Reflections library for classpath scanning
-    implementation("org.reflections:reflections:0.10.2")
+    api(rootProject.libs.reflections)
     
     // Kotlin reflection (usually provided by the Kotlin plugin, but explicitly declared for clarity)
     implementation(kotlin("reflect"))
