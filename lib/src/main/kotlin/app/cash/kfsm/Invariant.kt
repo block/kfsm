e3e2b62a82b386c1264d0c51faaa0e3929da2f1e
Jeremy Mawson <jmawson@block.xyz>
2025-05-03 14:45:00 +1000

    Remove commented out code in invariant dsl

--- lib/src/main/kotlin/app/cash/kfsm/Invariant.kt
+++ lib/src/main/kotlin/app/cash/kfsm/Invariant.kt
@@ -15,25 +15,3 @@ interface Invariant<ID, V : Value<ID, V, S>, S : State<ID, V, S>> {
      */
     fun validate(value: V): Result<V>
 }
-
-// class Invariants<ID, V : Value<ID, V, S>, S : State<S>> private constructor(
-//     private val invariants: List<Invariant<ID, V, S>>
-// ) : Invariant<ID, V, S> {
-//     override fun validate(value: V): Result<Unit> {
-//         for (invariant in invariants) {
-//             val result = invariant.validate(value)
-//             if (result.isFailure) {
-//                 return result
-//             }
-//         }
-//         return Result.success(Unit)
-//     }
-//
-//     companion object {
-//         fun <ID, V : Value<ID, V, S>, S : State<S>> of(
-//             vararg invariants: Invariant<ID, V, S>
-//         ): Invariants<ID, V, S> = Invariants(invariants.toList())
-//
-//         fun <ID, V : Value<ID, V, S>, S : State<S>> empty(): Invariants<ID, V, S> = Invariants(emptyList())
-//     }
-// }
