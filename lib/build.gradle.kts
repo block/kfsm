22b55265241e48bf89287e67fb046c31056365b4
Jem Mawson <jmawson@squareup.com>
2025-08-14 23:01:39 +1000

    Tidy up libs (#96)

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -20,12 +20,10 @@ mavenPublishing {
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.20")
-  implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.20")
-  
-  // Test dependencies
-  testImplementation("io.kotest:kotest-assertions-core:5.8.0")
-  testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
-  testImplementation("io.kotest:kotest-property:5.8.0")
-  testImplementation("io.mockk:mockk:1.13.10")
+  implementation(libs.kotlinReflect)
+
+  testImplementation(libs.kotestProperty)
+  testImplementation(libs.kotestAssertions)
+  testImplementation(libs.kotestJunitRunnerJvm)
+  testImplementation(libs.mockk)
 }
